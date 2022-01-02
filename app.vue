1eddfaa5e10942d8e6873c9552c20c0ea37750e0
Naim Zaid <naim@govicle.com>
2022-01-02 20:22:03 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Sidebar/>
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Test</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        info: {
+            type: String,
+            required: true
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
